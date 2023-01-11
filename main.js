c09bf4d44b5e935b3aea50f4389e2d5b50ad6254
naichilab <naichilab@live.jp>
2023-01-12 03:06:30 +0900

    作りかけ

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+function changed() {
+    alert("hoge")
+}
 
 function download() {
-    html2canvas(document.querySelector("#capture")).then(canvas => {
+    html2canvas(document.querySelector("#capture"), {
+        scale: 5
+    }).then(canvas => {
         let downloadEle = document.createElement("a");
         downloadEle.href = canvas.toDataURL("image/png");
         downloadEle.download = "canvas.png";
         downloadEle.click();
     });
-} 
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+});
